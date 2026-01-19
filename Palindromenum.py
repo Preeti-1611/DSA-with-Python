5ddf6e44d0c081872ae97ce9c3467d1887ff6384
Preeti-1611 <preetinagarale16@gmail.com>
2026-01-19 16:35:26 +0530

    solved Problem

---
+++ Palindromenum.py
@@ -0,0 +1,13 @@
+def isPenlindrome(num):
+    x = num
+    result = 0
+    while x >0 :
+        last = x%10
+        result = result*10+last
+        x = x//10
+    
+    return num == result
+
+
+num = 1211
+print(isPenlindrome(num))
